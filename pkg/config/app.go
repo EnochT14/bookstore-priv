8414987d29ff957eb59ad04ebe5290034b956a51
Enoch Cobbina <enochcobbina1@gmail.com>
2023-10-07 09:53:52 +0000

    first

---
+++ pkg/config/app.go
@@ -0,0 +1,24 @@
+package config
+
+import (
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/mysql"
+)
+
+var (
+	db *gorm.DB
+)
+
+func Connect() {
+	d, err := gorm.Open("mysql", "root:password/nameoftable?charset=utf8&parseTime=True&loc=Local")
+	if err != nil {
+		panic(err)
+	}
+	db = d
+}
+
+func GetDB() *gorm.DB {
+	return db
+}
+
+//purpose of this file is to return db variable that will help other files talk to the database
